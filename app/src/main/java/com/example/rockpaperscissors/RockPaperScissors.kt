c1526f70481a6ec907f7ca91d2ad13b22305fa6d
Christos <xristotsi@gmail.com>
2023-12-28 01:45:02 +0200

    .

---
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -0,0 +1,28 @@
+package com.example.rockpaperscissors
+
+fun main(){
+    var computerChoice = ""
+    var playerChoice = ""
+    println("Rock, Paper or Scissors? Enter your choice!")
+    playerChoice = readln()
+
+    val randomNumber = (1..3).random()
+    when (randomNumber) {
+        1 -> computerChoice= "Rock"
+        2 -> computerChoice = "Paper"
+        3 -> computerChoice= "Scissors"
+    }
+    println(computerChoice)
+
+    val winner = when{
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+    if (winner == "Tie")
+        println("Its a tie!")
+    else
+        println("The winner is the $winner")
+}
